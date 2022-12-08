5509dc0bd6df96b1485d607629a7b44b25dd69f0
tonmoyislam250 <su6087031@gmail.com>
2022-12-08 11:10:00 -0500

    hello
    Signed-off-by: tonmoyislam250 <su6087031@gmail.com>

--- push.py
+++ push.py
@@ -1,5 +1,5 @@
 import os
 os.system("python3 ./rmv.py")
-os.system("sudo git add -v .")
-os.system("sudo git commit -es")
-os.system("sudo git push")
+os.system("git add -v .")
+os.system("git commit -es")
+os.system("git push")
