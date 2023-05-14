038c299e38b122f29218225bfa146491f2a44bd0
tonmoyislam250 <tonmoyvyper@gmail.com>
2023-05-14 23:24:49 +0600

    H

---
+++ CSE 2102/lab5/q1/q1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
+void swap(long long *x, long long *y)
+{
+    long long temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void bubbleSort(long long arr[], long long n)
+{
+    long long i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+}
+void printarray(long long arr[], long long size)
+{
+    long long i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+}
+void genFile(long long arr[], long long n)
+{
+    srand((unsigned)time(NULL));
+    system("rm unsorteddata.txt");
+    ofstream hello("unsorteddata.txt");
+    for (long long i = 0; i < n; i++)
+        hello << rand() % 100 << endl;
+    hello.close();
+    ifstream hi("unsorteddata.txt");
+    for (int i = 0; i < n; i++)
+        hi >> arr[i];
+    // printarray(arr, n);
+    hi.close();
+}
+
+int main()
+{
+    long long n, offset;
+    cin >> n >> offset;
+
+    while (n > 0)
+    {
+        long long arr[n];
+        genFile(arr, n);
+        clock_t t;
+        t = clock();
+        bubbleSort(arr, n);
+        t = clock() - t;
+        cout << "Time taken: " << float(t) / CLOCKS_PER_SEC << " seconds "
+             << "for " << n << " Numbers" << endl;
+        n += offset;
+    }
+}
