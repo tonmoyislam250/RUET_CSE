2e87d7a2331f33c01e07db35720ac0c02925a723
tiktikimelbo7 <tiktikimelbo7@gmail.com>
2023-05-20 18:11:38 +0600

    J

---
+++ CSE 2102/Lab6/q1/q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void sieveE(int a)
+{
+    vector<int> p(a + 1, true);
+    fill(p.begin(), p.end(), 0);
+    for (int i = 2; i * i <= a; i++)
+    {
+        if (p[i] == 0)
+        {
+            for (int j = i * i; j <= a; j += i)
+                p[j] = 1;
+        }
+    }
+    for (int i = 2; i <= a; i++)
+        if (p[i] == 0)
+            cout << i << " ";
+}
+int main()
+{
+    int a;
+    cin >> a;
+    cout << "Prime numbers upto " << a << " are: " << endl;
+    sieveE(a);
+    return 0;
+}
---
+++ CSE 2102/Lab6/q2/q2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+#define MAX 100001
+void makespf(int a[], int n)
+{
+    a[1] = 1;
+    int ii = 0;
+    int b[n];
+    for (int i = 2; i < MAX; i++)
+        a[i] = i;
+    for (int j = 4; j < MAX; j += 2)
+        a[j] = 2;
+
+    for (int i = 3; i * i < MAX; i++)
+    {
+        if (a[i] == i)
+        {
+            for (int j = i * i; j < MAX; j += i)
+            {
+                if (a[j] == j)
+                    a[j] = i;
+            }
+        }
+    }
+    while (n != 1)
+    {
+        b[ii] = a[n];
+        ii++;
+        n = n / a[n];
+    }
+    for (int i = 0; i < ii; i++)
+        cout << b[i] << " ";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[MAX];
+    makespf(a, n);
+}
