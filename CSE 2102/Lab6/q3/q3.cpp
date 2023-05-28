2b2f77fdec72dbf8bcaa11b8261afc8117b60f81
tonmoyislam12 <merrydith52829@hotmail.com>
2023-05-28 09:58:57 +0600

    H

--- CSE 2102/Lab6/q3/q3.cpp
+++ CSE 2102/Lab6/q3/q3.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 using namespace std;
-
+int i = 1, j = 1;
 int extend(int a, int b, int &x, int &y)
 {
-    cout << "a: " << a << " b: " << b << endl;
-    if (b == 0)
-    {
-        x = 1;
-        y = 0;
-        return a;
-    }
-    int x1, y1;
-    int d = extend(b, a % b, x1, y1);
-    cout << "x1: " << x1 << " y1: " << y1 << endl;
-    x = y1;
-    y = x1 - y1 * (a / b);
-    cout << "x: " << x << " y: " << y << endl;
-    return d;
+	cout << i++ << " : "
+		 << "a: " << a << " b: " << b << endl;
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+	int x1, y1;
+	int d = extend(b, a % b, x1, y1);
+	cout << endl;
+	cout << j++ << " : "
+		 << "x1: " << x1 << " y1: " << y1 << endl;
+	cout << "a: " << a << " b: " << b << endl;
+	x = y1;
+	y = x1 - y1 * (a / b);
+	cout << "x: " << x << " y: " << y << endl;
+	return d;
 }
 int main()
 {
-    int a, b, x, y;
-    cin >> a >> b;
-    int g = extend(a, b, x, y);
-    cout << a << "*" << x << " + " << b << "*" << y << " = " << g << endl;
+	int a, b, x, y;
+	cin >> a >> b;
+	int g = extend(a, b, x, y);
+	cout << a << "*" << x << " + " << b << "*" << y << " = " << g << endl;
 }
