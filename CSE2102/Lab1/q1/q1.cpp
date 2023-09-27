0bf1a55ac221de500898efd1cc06b63b96c9839d
tonmoy <tonmoyislam250>
2023-09-27 09:51:20 +0600

    First Commit

---
+++ CSE2102/Lab1/q1/q1.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <chrono>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "sum = " << a + b << endl;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    auto start = std::chrono::high_resolution_clock::now();
+    fast_cin();
+    long long t;
+    cin >> t;
+    for (int it = 0; it < t; it++)
+    {
+        cout << "Case #" << it + 1 << ": ";
+        solve();
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Run time: " << duration.count() << " microseconds" << std::endl;
+    return 0;
+}
